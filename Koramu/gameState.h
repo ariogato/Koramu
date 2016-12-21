8d3e69adee0cd9ba6c81e23619e52ed5a3a18911
ariogato <ariodddddd@yahoo.de>
2016-12-21 22:23:26 +0100

    #8 Reviewed Ticket & Kommentare hinzugefügt

--- Koramu/gameState.h
+++ Koramu/gameState.h
@@ -1,28 +1,46 @@
 #pragma once
 
+/*	In diesen namespace gehˆrt alles, was mit dem Zustandsautomaten
+*	des Spiels zu tun hat.
+*/
 namespace FiniteStateMachine 
 {
+	/*	Dies ist ein Datentyp f¸r unsere Zust‰nde.
+	*	Neue Zust‰nde m¸ssen erstmal in der enum definiert werden.
+	*/
 	enum class GameStateIDs
 	{
 		menuState, playState, pauseState
 	};
 
 
+	/*	Diese Klasse ist Basis f¸r alle Zust‰nde, die 
+	*	man in unserem Spiel erreichen kann.
+	*	Sie stellt das "Datenelement" in unserem Entwurfsmuster
+	*	des Kompositums f¸r einen Stapel von Spielzust‰nden dar	
+	*/
 	class GameState
 	{
 	private:
-		GameStateIDs stateID;
+		GameStateIDs m_stateID;						//	Um den Zustand identifizieren zu kˆnnen
+
+	protected:
+		GameState() {}
+		virtual ~GameState() {}
+		
+		/*	Der Destruktor sollte virtual gesetzt werden,
+		*	um  undefiniertes Verhalten zu verhindern.
+		*	F¸r mehr Info, siehe unser Wiki (Cpp Tipps -> Abstrakte Klassen)
+		*/
+
 	public:
-		GameState();
-		virtual ~GameState();
+		virtual void onEnter() = 0;					//	Wird aufgerufen sobald der Zustand eintritt
+		virtual void onExit() = 0;					//	Wird aufgerufen sobald der Zustand verlassen wird
+		virtual void handleInput() = 0;				//	Eingaben bearbeiten
+		virtual void update() = 0;					//	"Back-End"-Sachen (basierend auf den Eingaben) berechnen
+		virtual void render() = 0;					//	Alles auf den Bildschirm schmeiﬂen
 
-		virtual void onEnter() = 0;
-		virtual void onExit() = 0;
-		virtual void handleInput() = 0;
-		virtual void update() = 0;
-		virtual void render() = 0;
-		virtual void getState() = 0;
+		//	getter-Funktionen
+		GameStateIDs getStateID() { return m_stateID; }
 	};
-
-	
 };
