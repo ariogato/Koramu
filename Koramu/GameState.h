5eeeaa29ea0d1d9cb1bbab054823b7178afc1840
Ario Dastmaltschi <ariogato@users.noreply.github.com>
2017-01-19 19:01:18 +0100

    Rename gameState.h to GameState.h

--- Koramu/GameState.h
+++ Koramu/GameState.h
@@ -1,12 +1,12 @@
 #pragma once
 
-/*	In diesen namespace gehˆrt alles, was mit dem Zustandsautomaten
+/*	In diesen namespace geh√∂rt alles, was mit dem Zustandsautomaten
 *	des Spiels zu tun hat.
 */
 namespace FiniteStateMachine 
 {
-	/*	Dies ist ein Datentyp f¸r unsere Zust‰nde.
-	*	Neue Zust‰nde m¸ssen erstmal in der enum definiert werden.
+	/*	Dies ist ein Datentyp f√ºr unsere Zust√§nde.
+	*	Neue Zust√§nde m√ºssen erstmal in der enum definiert werden.
 	*/
 	enum class GameStateIDs
 	{
@@ -14,15 +14,15 @@ namespace FiniteStateMachine
 	};
 
 
-	/*	Diese Klasse ist Basis f¸r alle Zust‰nde, die 
+	/*	Diese Klasse ist Basis f√ºr alle Zust√§nde, die 
 	*	man in unserem Spiel erreichen kann.
 	*	Sie stellt das "Datenelement" in unserem Entwurfsmuster
-	*	des Kompositums f¸r einen Stapel von Spielzust‰nden dar	
+	*	des Kompositums f√ºr einen Stapel von Spielzust√§nden dar	
 	*/
 	class GameState
 	{
 	private:
-		GameStateIDs m_stateID;						//	Um den Zustand identifizieren zu kˆnnen
+		GameStateIDs m_stateID;						//	Um den Zustand identifizieren zu k√∂nnen
 
 	protected:
 		GameState() {}
@@ -30,7 +30,7 @@ namespace FiniteStateMachine
 		
 		/*	Der Destruktor sollte virtual gesetzt werden,
 		*	um  undefiniertes Verhalten zu verhindern.
-		*	F¸r mehr Info, siehe unser Wiki (Cpp Tipps -> Abstrakte Klassen)
+		*	F√ºr mehr Info, siehe unser Wiki (Cpp Tipps -> Abstrakte Klassen)
 		*/
 
 	public:
@@ -38,7 +38,7 @@ namespace FiniteStateMachine
 		virtual void onExit() = 0;					//	Wird aufgerufen sobald der Zustand verlassen wird
 		virtual void handleInput() = 0;				//	Eingaben bearbeiten
 		virtual void update() = 0;					//	"Back-End"-Sachen (basierend auf den Eingaben) berechnen
-		virtual void render() = 0;					//	Alles auf den Bildschirm schmeiﬂen
+		virtual void render() = 0;					//	Alles auf den Bildschirm schmei√üen
 
 		//	getter-Funktionen
 		GameStateIDs getStateID() { return m_stateID; }
