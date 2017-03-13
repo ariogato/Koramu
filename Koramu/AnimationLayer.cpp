d9ef9fd748d45cef76ef31c1d2c868ca0fe06363
Roman Mayr <roman.mayr@gmx.net>
2017-03-13 22:19:20 +0100

    Von "Layer" erbende Klasse "AnimationLayer" hinzugefügt.

---
+++ Koramu/AnimationLayer.cpp
@@ -0,0 +1,17 @@
+#include "AnimationLayer.h"
+
+Environment::AnimationLayer::AnimationLayer()
+{
+}
+
+Environment::AnimationLayer::~AnimationLayer()
+{
+}
+
+void Environment::AnimationLayer::update()
+{
+}
+
+void Environment::AnimationLayer::render()
+{
+}
---
+++ Koramu/AnimationLayer.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Layer.h"
+#include "vector"
+
+namespace Environment
+{
+	/*	Diese Klasse managed unsere Animationen.
+	*/
+	class AnimationLayer : public Layer
+	{
+	private:
+		//	std::vector<Anímation*> m_animations TODO: Klasse "Animation" implementieren
+
+	public:
+		AnimationLayer();
+		~AnimationLayer();
+
+		void update();							//	Aktualisiern
+		void render();							//	Rendern
+	};
+
+
+}
