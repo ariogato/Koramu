5b6b135b0a118a602a6392461d222d042be9ea70
Roman Mayr <roman.mayr@gmx.net>
2018-01-25 20:05:12 +0100

    #68 zweite Quest implementiert.

--- Koramu/Scripts/Objects/NPCs/clockmaker.lua
+++ Koramu/Scripts/Objects/NPCs/clockmaker.lua
@@ -7,8 +7,9 @@ function clockmaker:interact (n, p)
 	interact.pl = p
 	interact.np = n
 	if TheGame:getPartQuestId() == "findClockmaker" then
-		TheGame:startDialog ("Excuse me Sir ...\nYou don't belong here, that's clear to see. What is it you want?\nWell my watch has stopped and I came to ask you to fix it for me.\nIs that so? Mmh... that is a wonderful watch. I guarantee it will work better than ever before if you give it to me. But it will take some\ntime to fix it. You can come back in half an hour at the earliest. And now don't disturb me any longer, I will go to work immediately.",
+		TheGame:startDialog ("Cornelius: Excuse me Sir ...\nClockmaker: You don't belong here, that's clear to see. What is it you want?\nCornelius: Well my watch has stopped and I came to ask you to fix it for me.\nClockmaker: Is that so? Mmh... that is a wonderful watch. I guarantee it will work better than ever before if you give it to me.\nBut it will take some time to fix it. You can come back in half an hour at the earliest.\nAnd now don't disturb me any longer, I will go to work immediately.",
 		interact.np, interact.pl)
+		TheGame:nextQuest()
 	end
 end
 
--- Koramu/Scripts/Quests/fixWatch/findClockmaker.lua
+++ Koramu/Scripts/Quests/fixWatch/findClockmaker.lua
@@ -5,7 +5,7 @@ local findClockmakerShop = {}
 function findClockmakerShop:onStart ()
 	player = {}
 	player.instance = Player.getInstance ()
-	TheGame:startNarrator ("How strange, the is still nobody to be seen!\nI think I will have another look around.\n Maybe I will at least find a place to get my watch fixed.", player.instance)
+	TheGame:startNarrator ("How strange, there is still nobody to be seen!\nI think I will have another look around.\n Maybe I will at least find a place to get my watch fixed.", player.instance)
 end
 
 
