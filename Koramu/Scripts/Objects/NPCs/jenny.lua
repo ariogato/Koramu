4c47e2f9df69e9ca048cdd0335554cd3f978e727
ariogato <ario2000.ad@gmail.com>
2018-01-26 00:05:16 +0100

    Bugfixes

--- Koramu/Scripts/Objects/NPCs/jenny.lua
+++ Koramu/Scripts/Objects/NPCs/jenny.lua
@@ -11,7 +11,7 @@ function jenny:onCollision ()
 	if TheGame:getPartQuestId () == "talkToJenny" then
 		TheGame:startDialog ("Girl: I am sorry for bumping into you, Sir.\nI was deeply in thought, my mother is very ill and I am worried about her so I decided to take a walk and...\nCornelius: Never mind. I am so glad to finally meet somebody who is less grumpy than that old clockmaker whom I left my watch for repair.",
 		characters.jenny, characters.player)
-		TheGame:startDialog ("Girl: He is not grumpy, he just doesn't like strangers. Don't take it personally.\nBy the way, his watches are very popular in town. Nearly everyone here owns at least one peace of his making.\nI wish I could afford one myself. ... Oh, I think I haven't introduced myself yet: I am Jenny.",
+		TheGame:startDialog ("Girl: He is not grumpy, he just doesn't like strangers. Don't take it personally.\nBy the way, his watches are very popular in town. Nearly everyone here owns at least one piece of his making.\nI wish I could afford one myself. ... Oh, I think I haven't introduced myself yet: I am Jenny.",
 		characters.jenny, characters.player)
 		TheGame:startDialog ("Cornelius: Nice to meet you, Jenny. My name is Cornelius. I've just arrived here this morning and don't know anything about this town.\nWould you mind telling me more about it?\nJenny: Well, they say the clockmaker's shop was the first building in town, but nobody knows how old he really is\nor how long he has been here. My mother has known him since she was a child.",
 		characters.jenny, characters.player)
